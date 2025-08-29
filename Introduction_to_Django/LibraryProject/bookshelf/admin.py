84ac7bb417ccfefbc619e5792a5d874e1192aaf7
shariz <benjahshariz@gmail.com>
2025-08-29 19:23:13 +0300

    Utilizing the Django Admin Interface
    Registered the Book model in the Django admin and customized the interface to display title, author, and publication_year. Added search and filter functionalities. Created a superuser to access and manage book entries via the admin interface.

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    # Columns shown in the list view
+    list_display = ('title', 'author', 'publication_year')  
+    # Search bar for title & author
+    search_fields = ('title', 'author')                     
+    # Filter sidebar by year
+    list_filter = ('publication_year',)
+
+admin.site.register(Book, BookAdmin)
