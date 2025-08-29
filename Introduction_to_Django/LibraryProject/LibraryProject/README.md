7e442c4ca68ec0227cf7152b33edd8f6e8dd6bba
shariz <benjahshariz@gmail.com>
2025-08-29 18:36:37 +0300

    Setup Django development environment
    - Installed Django and verified installation
    - Created new project `LibraryProject`
    - Ran development server and confirmed welcome page
    - Added README.md with basic project description

--- Introduction_to_Django/LibraryProject/LibraryProject/README.md
+++
@@ -1,3 +0,0 @@
-# LibraryProject
-
-This is my first Django project, created as part of the Introduction to Django task.
--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pu*^kbd2-@)nc7pri!i*5l4ftx96xy1f%i)pcw^x=qse14we3!'
+SECRET_KEY = 'django-insecure-$)+#d*320v2)+$b=fv5t+z)qrmtpoi%fm!@)@lazhobo1gk-xm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
