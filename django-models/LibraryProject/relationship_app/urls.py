e5cbd1dfbc0f03d64c569113158b7f48c4bdb734
shariz <benjahshariz@gmail.com>
2025-09-07 23:44:09 +0300

    updated urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -16,7 +16,8 @@ urlpatterns = [
     path("member-view/", views.member_view, name="member_view"),
 
     # Task 4: Book Permission-Based URLs
-    path("book/add/", views.add_book, name="add_book"),
-    path("book/<int:book_id>/edit/", views.edit_book, name="edit_book"),
-    path("book/<int:book_id>/delete/", views.delete_book, name="delete_book"),
+    path("add_book/", views.add_book, name="add_book"),
+    path("edit_book/<int:book_id>/", views.edit_book, name="edit_book"),
+    path("delete_book/<int:book_id>/", views.delete_book, name="delete_book"),
+
 ]
