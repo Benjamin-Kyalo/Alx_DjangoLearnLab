1a6acf6e091024007fdbf81c8d78bc30cd69bf10
shariz <benjahshariz@gmail.com>
2025-09-14 13:03:55 +0300

    Managing Permissions and Groups in Django

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -51,3 +51,23 @@ class CustomUser(AbstractUser):
     def __str__(self):
         # string representation in admin and shell
         return self.username
+
+
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+    class Meta:
+        # STEP 1: Define custom permissions
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
-# Create your views here.
+
+# STEP 3.1: Only users with "can_view" can see the book list
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()  # safe query
+    return render(request, "bookshelf/book_list.html", {"books": books})
+
+
+# STEP 3.2: Only users with "can_create" can add a book
+@permission_required('bookshelf.can_create', raise_exception=True)
+def add_book(request):
+    # just a placeholder view for now
+    return render(request, "bookshelf/form_example.html")
+
+
+# STEP 3.3: Only users with "can_edit" can edit a book
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    # just a placeholder view
+    return render(request, "bookshelf/form_example.html")
